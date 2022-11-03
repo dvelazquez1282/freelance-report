492b6de561e041a2943ebc5901e81323cbc52c9a
Daniel Velázquez <dvelazquez1282@gmail.com>
2022-11-03 16:14:12 +0100

    - Added model for timing

---
+++ src/app/timing/models/timing.model.ts
@@ -0,0 +1,9 @@
+export class Model {
+    public id: number;
+    public time: number;
+    public task: string;
+    public description: string;
+    public created_at: string;
+    public customer_id: number;
+    public type_id: number;
+}
